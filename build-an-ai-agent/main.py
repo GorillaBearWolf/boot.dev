c13b78c00e60a4b505a4dcd491f7f8ebe6f4de41
Matthew Refsnider <mrefsnider@icloud.com>
2025-08-26 00:42:26 -0400

    wip C2L1

--- build-an-ai-agent/main.py
+++ build-an-ai-agent/main.py
@@ -2,29 +2,39 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
-
-load_dotenv()
-
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+from google.genai import types
 
 
-def main(argv):
-    try:
-        response = client.models.generate_content(
-            model="gemini-2.0-flash-001", contents=argv[1]
-        )
+def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("Error, exiting")
+        sys.exit(1)
+    user_prompt = args[0]
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+
+    if " ".join(args).endswith("--verbose"):
         ptokens = response.usage_metadata.prompt_token_count
         rtokens = response.usage_metadata.candidates_token_count
 
         print(
-            f"{response.text}\n\nPrompt tokens: {ptokens}\nResponse tokens: {rtokens}"
+            f"User prompt: {user_prompt}\nPrompt tokens: {ptokens}\nResponse tokens: {rtokens}\n"
         )
-
-    except IndexError:
-        print("Error, exiting")
-        exit(1)
+    print(f"Response text: {response.text}")
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
